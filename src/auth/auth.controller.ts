662c6ed6429a64836635dccb8afdb6509038a148
shvvkz <viggocasciano0301@gmail.com>
2025-01-22 16:26:52 +0100

    finished

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Get,
+  Request,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginAuthDto } from './dto/login-auth.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -8,6 +16,17 @@ export class AuthController {
 
   @Post('login')
   async login(@Body() loginDto: LoginAuthDto) {
-    return this.authService.login(await this.authService.validateUser(loginDto.username, loginDto.password));
+    return this.authService.login(
+      await this.authService.validateUser(loginDto.username, loginDto.password),
+    );
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('validate-token')
+  validateToken(@Request() req: any) {
+    return {
+      valid: true,
+      user: req.user,
+    };
   }
 }
